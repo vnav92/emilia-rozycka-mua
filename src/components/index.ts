0f17ebcdac4c3718307127e47f17bf2176cf7952
oeh_aud <vortexflameout@gmail.com>
2022-07-04 01:25:37 +0200

    Resolve conflicts

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,8 +5,5 @@ export * from "./layout";
 export * from "./limited-width-content";
 export * from "./navbar-link";
 export * from "./redirection-link";
-<<<<<<< HEAD
 export * from "./section-header";
-=======
->>>>>>> 050ab538f01d34eccc61c5f2f1b3a3b915b16203
 export * from "./typography";
--- src/components/redirection-link/redirection-link.component.tsx
+++ src/components/redirection-link/redirection-link.component.tsx
@@ -4,14 +4,9 @@ import { Link } from "gatsby";
 
 import { BiRightArrowAlt } from "react-icons/bi";
 
-<<<<<<< HEAD
 import { Typography, TypographyColor } from "../typography";
 
 import * as styles from "./redirection-link.module.scss";
-=======
-import * as styles from "./redirection-link.module.scss";
-import { Typography, TypographyColor } from "../typography";
->>>>>>> 050ab538f01d34eccc61c5f2f1b3a3b915b16203
 
 type RedirectionLink = {
   to: string;
