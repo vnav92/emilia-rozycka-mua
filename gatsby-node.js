7a72028c02c33d70dc886072d7b6227a41b7eaa6
vnav92 <vortexflameout@gmail.com>
2022-10-07 14:25:09 +0200

    Fetch more data from wordpress

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,26 @@
 exports.createPages = async ({ actions, graphql }) => {
   const { data } = await graphql(`
-    query SlugsQuery {
-      allWpPost(
-        filter: {
-          categories: { nodes: { elemMatch: { name: { eq: "offer-item" } } } }
-        }
-      ) {
-        edges {
-          node {
-            slug
-            offerItem {
-              primarytitle
-              secondarytitle
-            }
+  query SlugsQuery {
+    allWpPost(
+      filter: {categories: {nodes: {elemMatch: {name: {eq: "offer-item"}}}}}
+    ) {
+      edges {
+        node {
+          slug
+          offerItem {
+            primarytitle
+            secondarytitle
+            firstitemcontent
+            firstitemprice
+            firstitemtitle
+            secondItemContent
+            seconditemprice
+            seconditemtitle
           }
         }
       }
     }
+  }
   `);
 
   data.allWpPost.edges.forEach(({ node }) => {
