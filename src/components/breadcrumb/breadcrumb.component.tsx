5d5d19384101ce1707b6653ff2b71e4bb551c9e3
Artur <tuchowskyy@gmail.com>
2022-10-10 15:03:06 +0200

    (styles): style breadcrub element

--- src/components/breadcrumb/breadcrumb.component.tsx
+++ src/components/breadcrumb/breadcrumb.component.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { useLocation, Link } from "@reach/router";
+import classNames from "classnames";
 
 import * as styles from "./breadcrumb.module.scss";
 
 type BreadCrumbProps = {
   lastItemTitle: string;
+  className?: string;
 };
 
 const LINKS_SEPARATOR = "/";
 
 // Initial version of breadcrumb which applies only to offer-item
 // Will be improved if needed
-export const Breadcrumb: React.FC<BreadCrumbProps> = ({ lastItemTitle }) => {
+export const Breadcrumb: React.FC<BreadCrumbProps> = ({ lastItemTitle, className }) => {
   const { pathname } = useLocation();
 
   return (
-    <div className={styles.breadcrumbWrapper}>
+    <div className={classNames(styles.breadcrumbWrapper, className)}>
       <Link to="/">Home</Link>
       <span>{LINKS_SEPARATOR}</span>
       <Link to="/offer">Oferta</Link>
