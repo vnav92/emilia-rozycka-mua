9343848ff610bb2e9cd6e9fe258bd8a5a4cff4a2
Artur <tuchowskyy@gmail.com>
2022-10-16 22:37:28 +0200

    (config): update config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Emilia Różycka MUA`,
+    title: `Emilia Różycka Make Up Artist`,
     author: "Artur Tuchowski & Mateusz Jankowski",
-    description: "",
+    description: "Emilia Różycka Make Up Artist - makijażystka Gdańsk, Trójmiasto, woj. Pomorskie. Makijaż ślubny, okolicznościowy, biznesowy, do sesji zdjęciowych. Lekcje makijażu, szkolenia.",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Emilia Różycka MUA`,
+        name: `Emilia Różycka Make Up Artist`,
         short_name: `MUA`,
         start_url: `/`,
         background_color: `#663399`,
